625760f230a962eba0a4e80ab662b922eca241d1
almirpask <almirpask@hotmail.com>
2024-01-30 00:32:17 -0300

    add main

---
+++ internal/cmd/catalog/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/almirpask/go_api/internal/database"
+	"github.com/almirpask/go_api/internal/service"
+	"github.com/almirpask/go_api/internal/webserver"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/catalog")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategoryDB(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+
+	productDB := database.NewProductDB(db)
+	productService := service.NewProductService(*productDB)
+
+	webCategoryHandler := webserver.NewWebCategoryHandler(categoryService)
+	webProductHandler := webserver.NewWebProductHandler(productService)
+
+	c := chi.NewRouter()
+
+	c.Use(middleware.Logger)
+	c.Use(middleware.Recoverer)
+
+	c.Get("/category/{id}", webCategoryHandler.GetCategory)
+	c.Get("/category", webCategoryHandler.GetCategories)
+	c.Post("/category", webCategoryHandler.CreateCategory)
+
+	c.Get("/product/{id}", webProductHandler.GetProduct)
+	c.Get("/product", webProductHandler.GetProducts)
+	c.Get("/product/category/{categoryID}", webProductHandler.GetProductByCategoryID)
+	c.Post("/product", webProductHandler.CreateProduct)
+
+	fmt.Println("Server running on port 8080")
+	http.ListenAndServe(":8080", c)
+}
