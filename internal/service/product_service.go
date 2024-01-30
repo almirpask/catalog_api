65261fb3c7bea5fb331ee9f90c39b8f93c8d0aef
almirpask <almirpask@hotmail.com>
2024-01-29 23:23:41 -0300

    add product service

---
+++ internal/service/product_service.go
@@ -0,0 +1,58 @@
+package service
+
+import (
+	"github.com/almirpask/go_api/internal/database"
+	"github.com/almirpask/go_api/internal/entity"
+)
+
+type ProductService struct {
+	ProductDB database.ProductDB
+}
+
+func NewProduct(productDb database.ProductDB) *ProductService {
+	return &ProductService{
+		ProductDB: productDb,
+	}
+}
+
+func (ps *ProductService) GetProducts() ([]*entity.Product, error) {
+	products, err := ps.ProductDB.GetProducts()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
+
+func (ps *ProductService) CreateProduct(name string, description string, price float64, categoryID string, imageURL string) (*entity.Product, error) {
+	product := entity.NewProduct(name, description, price, categoryID, imageURL)
+
+	_, err := ps.ProductDB.CreateProduct(product)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (ps *ProductService) GetProduct(id string) (*entity.Product, error) {
+	product, err := ps.ProductDB.GetProduct(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (ps *ProductService) GetProductByCategoryID(categoryID string) ([]*entity.Product, error) {
+	products, err := ps.ProductDB.GetProductByCategoryID(categoryID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
