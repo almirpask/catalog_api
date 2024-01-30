9f4ebda5d6fd6830d80bcc23a9604ab777a992b4
almirpask <almirpask@hotmail.com>
2024-01-29 23:23:10 -0300

    add category service

---
+++ internal/service/category_service.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"github.com/almirpask/go_api/internal/database"
+	"github.com/almirpask/go_api/internal/entity"
+)
+
+type CategoryService struct {
+	CategoryDb database.CategoryDB
+}
+
+func NewCategory(categoryDb database.CategoryDB) *CategoryService {
+	return &CategoryService{
+		CategoryDb: categoryDb,
+	}
+}
+
+func (cs *CategoryService) GetCategories() ([]*entity.Category, error) {
+	categories, err := cs.CategoryDb.GetCategories()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return categories, nil
+}
+
+func (cs *CategoryService) CreateCategory(name string) (*entity.Category, error) {
+	category := entity.NewCategory(name)
+
+	_, err := cs.CategoryDb.CreateCategory(category)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return category, nil
+}
+
+func (cs *CategoryService) GetCategory(id string) (*entity.Category, error) {
+	category, err := cs.CategoryDb.GetCategory(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return category, nil
+}
