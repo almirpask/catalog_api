b398c2326cc06862c382e27f03c08d8df356123b
almirpask <almirpask@hotmail.com>
2024-01-30 00:41:09 -0300

    setup mysql

--- internal/cmd/catalog/main.go
+++ internal/cmd/catalog/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/almirpask/go_api/internal/webserver"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
