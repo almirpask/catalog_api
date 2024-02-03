4cac5a056dc95d6b984b9268568d4682e5718214
almirpask <almirpask@hotmail.com>
2024-02-03 12:07:54 -0300

    update db host

--- internal/cmd/catalog/main.go
+++ internal/cmd/catalog/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/catalog")
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/catalog")
 	if err != nil {
 		panic(err)
 	}
